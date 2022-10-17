2ffc6c6a22fc2da66e11a58bc483fc0e37bf2100
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2022-10-17 19:36:07 -0400

    added context

--- files/Source/PluginProcessor.cpp
+++ files/Source/PluginProcessor.cpp
@@ -148,6 +148,16 @@ void EqAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::Mid
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
+    juce::dsp::AudioBlock<float> block(buffer);
+    
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+    
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+    
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
 
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
